7a7f62219ce73c019ddffd36100414657447f0af
MarcoMorales-77 <2011@holbertonschool.com>
2020-06-17 17:27:41 +0000

    I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include"holberton.h"
+/**
+*jack_bauer - prints all minutes and hours of a day
+*Return: Always 0 (Success)
+*/
+void jack_bauer(void)
+{
+int min, hr;
+for (hr = 0; hr <= 23; hr++)
+{
+for (min = 0; min <= 59; min++)
+{
+_putchar((hr / 10) + '0');
+_putchar((hr % 10) + '0');
+_putchar(58);
+_putchar((min / 10) + '0');
+_putchar((min % 10) + '0');
+_putchar('\n');
+}
+}
+}
