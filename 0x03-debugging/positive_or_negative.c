7ea16dbe747f690ce1fc5c8f0bc18c543bc5ebc6
MarcoMorales-77 <2011@holbertonschool.com>
2020-06-19 14:56:51 +0000

    Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,19 @@
+  
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * positive_or_negative - clasify an integer
+ * @n: integer to clasify
+ * Return: Always 0 (Success)
+ */
+int positive_or_negative(int n)
+{
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
+
+}
