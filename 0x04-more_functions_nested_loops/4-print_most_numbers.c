53541de77fbc12ace076174798db87df4c2ff4ad
MarcoMorales-77 <2011@holbertonschool.com>
2020-06-19 16:34:41 +0000

    I bilieve in numberes and sings

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* print_most_numbers - check the code for Holberton School students.
+* Disc: Entry
+* Return: Always 0.
+*/
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = '0' ; n <= '9' ; n++)
+	{
+
+		if (n != '2' && n != '4')
+		{
+			_putchar (n);
+		}
+	}
+	_putchar ('\n');
+}
