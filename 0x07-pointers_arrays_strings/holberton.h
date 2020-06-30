24d00a4c3f86ae483de256ef0485ac7113c40c30
MarcoMorales-77 <2011@holbertonschool.com>
2020-06-30 21:44:21 +0000

    strchr

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -5,5 +5,6 @@
 int _putchar(char c); /*_putchar*/
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif /* HOLBERTON_H */
