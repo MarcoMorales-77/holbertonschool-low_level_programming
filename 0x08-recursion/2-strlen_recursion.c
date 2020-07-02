455ce409f5ad6954e49618003e4f2030e15e21f1
MarcoMorales-77 <2011@holbertonschool.com>
2020-07-02 15:22:59 +0000

    Dreams feel real while we're in them. it's only when we wake up that we realize something was actually strange

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* _strlen_recursion - prints pointer string length.
+*@s: - Declaration of variable
+*Return: Void
+*/
+
+int _strlen_recursion(char *s)
+
+{
+
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(++s));
+	}
+	else
+	{
+		return (0);
+	}
+
+}
